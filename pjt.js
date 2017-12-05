497812a5c9eefc7591253691a05244f52cda4c41
pjtsearch <piwithewiwi@gmail.com>
2017-12-04 16:49:17 -0800

    Update pjt.js

--- pjt.js
+++ pjt.js
@@ -1,22 +1,5 @@
-var P = function(element){return document.querySelector(element);}
-HTMLElement.prototype.html = function(html){
-    this.innerHTML = html;
-};
-P("#a").html("test");
-HTMLElement.prototype.css = function(styles){
-    this.style.cssText += styles;
-};
-P("#a").css("color:red; text-align:center");
-/*nction css(css){
-  var selector=css.split('{')[0];
-console.log(selector);
-var csstext=css.substring(css.lastIndexOf("{")+1,css.lastIndexOf("}"));
-var csstext = csstext.replace(/(\r\n|\n|\r)/gm,"");
-console.log(csstext);
-document.querySelector(selector).style.cssText += csstext;
-}*/
-var P = {};
-P.css = 
+var PCSS = {};
+PCSS.css = 
   function(css){
   var clause = css.replace(/(\r\n|\n|\r)/gm,"");
   var newstringreplaced = clause.replace(/}/gi, "},");
@@ -34,3 +17,13 @@ document.querySelector(selector).style.cssText += csstext;
   }
  }
 ;
+
+
+
+var P = function(element){return document.querySelector(element);}
+HTMLElement.prototype.html = function(html){
+    this.innerHTML = html;
+};
+HTMLElement.prototype.css = function(styles){
+    this.style.cssText += styles;
+};
